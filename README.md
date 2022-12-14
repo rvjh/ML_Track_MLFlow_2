1f00956c89ddae7c0f4554dd06757ebf18a9de96
rohan <mailtorohan.97@gmail.com>
2022-12-15 01:34:15 +0530

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ML_Track_MLFlow_2
 
+#### create a requirements.txt file and write mlflow,xgboost,scikit-learn,jupyter,pandas,hyperopt,seaborn,and xgboost
+
+#### converted parquet to csv for easy processing
---
+++ parquet_to_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+"""
+par1 = pd.read_parquet("data/green_tripdata_2021-01.parquet")
+csv_output_1 = 'data/january.csv'
+par1.to_csv(csv_output_1, index=False)
+
+par2 = pd.read_parquet("data/green_tripdata_2021-02.parquet")
+csv_output_2 = 'data/february.csv'
+par1.to_csv(csv_output_2, index=False)
+"""
+df1 = pd.read_csv("data/january.csv")
+print(df1.head())
+
+
+print(df1.columns)
